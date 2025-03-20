dab3da2d29d1f48ab252ebb3dd58869900589c17
matheus <mizepefacul@outlook.com>
2025-03-20 15:55:27 -0300

    Update script.js

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,12 +1,36 @@
-function numero(value) {
-    document.getElementById("tela").value += value;
-}
+var n1 = "", n2 = "", op ="", resultado = "0"
 
+function numero(value) {
+    if (op === "") {
+        n1 += value; 
+    } else {
+        n2 += value; 
+    }
+    document.getElementById("tela").value = n1 + op + n2;
+}
+function operador(value){
+    if(n1 !== ""){
+        op = value;
+        document.getElementById("tela").value = n1 + op
+    }
+}
 function limpar() {
-    document.getElementById("tela").value = "";
-}
+        n1 = n2 = op = "";
+        resultado = "0";
+        document.getElementById("tela").value = "";
+    }
+
 function calcular(){
-    var resultado = eval(document.getElementById("tela").value);
-        document.getElementById("tela").value = resultado
-    
-}
+    if (n1 !== "" && n2 !== "" && op !== "") {
+                var num1 = Number(n1);
+                var num2 = Number(n2);
+
+                if (op === "+") resultado = num1 + num2;
+                if (op === "-") resultado = num1 - num2;
+                if (op === "*") resultado = num1 * num2;
+                if (op === "/") resultado = num1 / num2;
+
+                n1 = resultado.toString();
+                n2 = op = "";
+                document.getElementById("tela").value = n1;
+}}
